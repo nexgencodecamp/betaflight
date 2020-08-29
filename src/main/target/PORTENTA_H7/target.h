d6ebae0616152989a163a4e1561faf1719d08ffe
David Such <dsuch@reefwing.com.au>
2020-08-29 17:22:27 +1000

    Update target.h
    Fixed USABLE_TIMER_CHANNEL_COUNT

--- src/main/target/PORTENTA_H7/target.h
+++ src/main/target/PORTENTA_H7/target.h
@@ -109,6 +109,6 @@
 #define TARGET_IO_PORTJ 0xffff
 #define TARGET_IO_PORTK 0xffff
 
-#define USABLE_TIMER_CHANNEL_COUNT 14
+#define USABLE_TIMER_CHANNEL_COUNT 4
 
 #define USED_TIMERS  ( TIM_N(1) | TIM_N(2) | TIM_N(3) | TIM_N(5) | TIM_N(8) | TIM_N(12) )
